9fa2af76a4534cb1c248788216adb60dd5484086
Polaki Sai Kiran <121670156+KiranPolaki@users.noreply.github.com>
2025-05-18 07:15:02 +0000

    create error

--- 01_setup.py
+++ 01_setup.py
@@ -1,2 +1,2 @@
-print("This is Demo for connection jenkins")
+prin("This is Demo for connection jenkins")
 print("Seeing SCM the pool in action : only trigger when changes happen and after the time limit v2")
